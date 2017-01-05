6a2f1c68db3e68a37a966b41cd292213cf673627
RichardNTran <trannguyenlong1991@gmail.com>
2017-01-05 23:22:03 +0700

    resolve conflict

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,8 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      flash[:success] = t "user.success"
+      log_in @user
+      flash.now[:success] = t "user.success"
       redirect_to @user
     else
       render :new
