59df4bb53d4951211f9c00b7795376c3e2f0864a
RichardNTran <trannguyenlong1991@gmail.com>
2017-01-06 09:19:04 +0700

    Login basic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,4 @@
 module ApplicationHelper
-   # Returns the full title on a per-page basis.
   def full_title page_title
     base_title = t "sample_app_title"
     return page_title.empty? ? base_title : page_title + " | " + base_title
