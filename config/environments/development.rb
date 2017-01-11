da840261f45569c3023a65873ca316b80f998f93
RichardNTran <trannguyenlong1991@gmail.com>
2017-01-11 21:04:54 +0700

    push

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = "agile-atoll-57154.herokuapp.com"
+  host = "lit-woodland-81336.herokuapp.com"
   config.action_mailer.default_url_options = {host: host}
   # ActionMailer::Base.smtp_settings = {
   #   address: "smtp.sendgrid.net",
