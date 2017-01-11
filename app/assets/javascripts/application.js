502eecba3d9079e4af505053b39ffc7075d688d0
RichardNTran <trannguyenlong1991@gmail.com>
2017-01-11 21:47:50 +0700

    change

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,5 +24,5 @@
 $('#micropost_picture').bind('change', function() {
   var size_in_megabytes = this.files[0].size/1024/1024;
   if (size_in_megabytes > 5) {
-    alert(<%= t("micropost.maximum_size_alert")%>);
+    alert('<%= t("micropost.maximum_size_alert")%>');
   }});
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Rails.application.configure do
 
     config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = "https://agile-atoll-57154.herokuapp.com"
+  host = "https://lit-woodland-81336.herokuapp.com"
   config.action_mailer.default_url_options = {host: host}
   ActionMailer::Base.smtp_settings = {
     address:        "smtp.gmail.com",
